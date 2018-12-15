d989fd718f9029b81c9d6f8c208c9a22f5e053c3
davut.ozcan <davut.ozcan@accenture.com>
2018-12-15 18:57:54 +0300

    google profile saved to local storage and redirected to main page after login

--- src/assets/google-sign-in.js
+++ src/assets/google-sign-in.js
@@ -3,8 +3,17 @@
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
+  console.log("profile" , profile);
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  let googleProfile = {
+    id:profile.getId(),
+    email:profile.getEmail(),
+    imageUrl:profile.getImageUrl()
+  };
+  localStorage.setItem("googleProfile" , JSON.stringify(googleProfile));
+  console.log("location" , window.location);
+  window.location = "/";
 }
