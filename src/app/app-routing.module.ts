daea0e4556bf44b062246dd424a5b268069ac273
davut.ozcan <davut.ozcan@accenture.com>
2018-12-15 17:37:07 +0300

    routing to google sign in works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GoogleSignInComponent } from './google-sign-in/google-sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: GoogleSignInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleSignInComponent } from './google-sign-in/google-sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoogleSignInComponent
   ],
   imports: [
     BrowserModule,
