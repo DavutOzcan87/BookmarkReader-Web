5d5280e038d83b5655490d32c349ea38952c1e7f
davut.ozcan <davut.ozcan@accenture.com>
2018-12-23 13:56:21 +0300

    bookmarks displayed v1

--- src/app/web-unit-list/web-unit-list.component.ts
+++ src/app/web-unit-list/web-unit-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BookmarkService } from '../services/bookmark-service';
 import { LoginService } from '../services/login.service';
+import { WeblistViewModel } from './weblist-view-model';
 
 @Component({
   selector: 'app-web-unit-list',
@@ -8,14 +9,28 @@ import { LoginService } from '../services/login.service';
   styleUrls: ['./web-unit-list.component.css']
 })
 export class WebUnitListComponent implements OnInit {
+  model = new WeblistViewModel();
+  constructor(private bookmarkService: BookmarkService, private loginService: LoginService ) {
+  }
 
-  constructor(private bookmarkService: BookmarkService, private loginService: LoginService ) { }
 
   ngOnInit() {
     console.log('trying to read bookmarks');
     this.bookmarkService
     .getBookmarks(this.loginService.getLoginedUser().id)
-    .subscribe(bookmarks => console.log('bookmarks from service' , bookmarks));
+    .subscribe(bookmarks => this.updateBookmarks(bookmarks));
+  }
+  updateBookmarks(bookmarks: string[]): void {
+    console.log('bookmarks from service' , bookmarks);
+    this.model.bookmarks = bookmarks;
+  }
+
+  delete(bookmark: string): void {
+    console.log('called bookmark delete');
+    const index = this.model.bookmarks.indexOf(bookmark);
+    if(index >= 0) {
+      this.model.bookmarks.slice(index , 1);
+    }
   }
 
 }
